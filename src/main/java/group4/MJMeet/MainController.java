65faf85b2df719a345d23be3487d87d990c1bf60
jimin <jikun.6016@gmail.com>
2022-11-24 15:40:37 +0900

    DB 출력 Test, root url redirect login

---
+++ src/main/java/group4/MJMeet/MainController.java
@@ -0,0 +1,15 @@
+package group4.MJMeet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/login";
+    }
+}
--- src/main/java/group4/MJMeet/User/LoginController.java
+++ src/main/java/group4/MJMeet/User/LoginController.java
@@ -4,13 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import org.springframework.ui.Model;
+import lombok.RequiredArgsConstructor;
 
+
+@RequiredArgsConstructor
 @Controller
 public class LoginController {
 
+    private final UserRepository userRepository;
     @RequestMapping("/login")
-    @ResponseBody
-    public String login() {
-        return "login!!";
+    public String login(Model model) {
+
+        List<UserEntity> userEntityList = this.userRepository.findAll();
+        model.addAttribute("userList", userEntityList);
+
+        return "login";
     }
 }
