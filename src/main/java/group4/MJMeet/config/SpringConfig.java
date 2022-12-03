249723281c2f639dcf8e2ed0484bd7ef00fba89f
wu-seong <jws001228@naver.com>
2022-12-04 01:45:16 +0900

    RoomService, RoomRepository bean등록

--- src/main/java/group4/MJMeet/config/SpringConfig.java
+++ src/main/java/group4/MJMeet/config/SpringConfig.java
@@ -1,8 +1,11 @@
 package group4.MJMeet.config;
 
 import group4.MJMeet.repository.JpaMemberRepository;
+import group4.MJMeet.repository.JpaRoomRepository;
 import group4.MJMeet.repository.MemberRepository;
+import group4.MJMeet.repository.RoomRepository;
 import group4.MJMeet.service.MemberService;
+import group4.MJMeet.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,4 +30,14 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         return new JpaMemberRepository(em);
     }
+
+    @Bean
+    public RoomService RoomService(){
+        return new RoomService(roomRepository());
+    }
+    @Bean
+    public RoomRepository roomRepository(){
+        return new JpaRoomRepository(em);
+    }
+
 }
